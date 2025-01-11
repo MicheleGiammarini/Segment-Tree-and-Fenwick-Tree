936733c56603cd267d41e1f1aafe596ceaa7dddf
Michele Giammarini <Giammarinimichele@outlook.com>
2025-01-11 17:07:17 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+class SegmentTree:
+    def __init__(self, arr):
+        self.n = len(arr)
+        self.tree = [0] * (2 * self.n)
+        self.build(arr)
+
+    def build(self, arr):
+        for i in range(self.n):
+            self.tree[self.n + i] = arr[i]
+        for i in range(self.n - 1, 0, -1):
+            self.tree[i] = self.tree[i * 2] + self.tree[i * 2 + 1]
+
+    def update(self, idx, value):
+        idx += self.n
+        self.tree[idx] = value
+        while idx > 1:
+            idx //= 2
+            self.tree[idx] = self.tree[2 * idx] + self.tree[2 * idx + 1]
+
+    def query(self, left, right):
+        left += self.n
+        right += self.n
+        result = 0
+        while left <= right:
+            if left % 2 == 1:
+                result += self.tree[left]
+                left += 1
+            if right %
